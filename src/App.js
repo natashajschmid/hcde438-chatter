b777452b78b232e484fb754a907f6e7e40183f39
Natasha Schmid <njschmid@uw.edu>
2021-01-27 11:42:27 -0800

    Make a TextInput

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+import TextInput from './TextInput';
 
 function App() {
+  const [messages, setMessages] = useState([{text: 'Type in a message'}]);
+
   return (
     <div className="App">
       <header className="header">
         <div className="logo" />
         CHATTER
       </header>
+
+      <div className="message">
+        {messages[0].text}
+      </div>
+
+      <TextInput 
+        send={t=> setMessages([{text:t}])}
+      />
+
     </div>
   );
 }
